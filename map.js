4762c141701dd5051e61b5e06be807ee27b608c8
Diogo Alexandre Domingos Lopes <diogolopes285@gmail.com>
2025-02-07 17:03:16 +0000

    map

--- map.js
+++ map.js
@@ -1,22 +1,12 @@
-let users = [  
+let users = new Map([  
   { id: 1, name: 'Alice', city: 'Paris' },  
   { id: 2, name: 'Bob', city: 'London' },  
   { id: 3, name: 'Charlie', city: 'Paris' }  
-];
+]);
+
+users = Object.groupBy(users, ({ city }) => city);
+console.log(users)
 
 
-let usersmap = new Map();
-users.forEach((key) => usersmap.set(value.city));
-console.info(usersmap);
 
 
-
-function deepEqual(obj1, obj2) {  
-    if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {  
-      return obj1 === obj2;  
-    }  
-    const keys1 = Object.keys(obj1);  
-    const keys2 = Object.keys(obj2);  
-    if (keys1.length !== keys2.length) return false;  
-    return keys1.every(key => deepEqual(obj1[key], obj2[key]));  
-  }  
