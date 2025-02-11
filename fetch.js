e2959330a4fccd7e009f246b0652f97d45e4e12d
DiogoLpes <diogolopes285@gmail.com>
2025-02-11 21:53:24 +0000

    session8

--- fetch.js
+++ fetch.js
@@ -1,24 +1,28 @@
+const API_KEY = "3cdab82538b633c1ec33f2e98e9d397f";
 
-const API_KEY = '3cdab82538b633c1ec33f2e98e9d397f';  
+async function weather(city) {
+  try {
+    const response = await fetch(
+      ` https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`,
+    );
+    if (!response.ok) throw new Error("Failed to fetch");
+    const data = await response.json();
+    const data = {
+      name: data.name,
+      temp: data.main.temp,
+      humidity: data.main.humidity,
+    };
+    return data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
-async function weather(city) {  
-  try {  
-    const response = await fetch(` https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);  
-    if (!response.ok) throw new Error('Failed to fetch');  
-    return await response.json();  
-  } catch (error) {  
-    console.error(error.message);  
-  }  
-} 
-
-async function fetchcitiesweather() {   
-    const cities = ['London', 'Paris', 'Tokyo'];  
-    const promises = cities.map((city) => weather(city));  
-    const users = await Promise.all(promises);  
-    console.log(users);  
-}  
-
-filteredData = new Map (data);
-
+async function fetchcitiesweather() {
+  const cities = ["London", "Paris", "Tokyo"];
+  const promises = cities.map((city) => weather(city));
+  const users = await Promise.all(promises);
+  WriteFileSync('weather2.json', JSON.stringify(weather, null, 2 ));
+}
 
 fetchcitiesweather();
---
+++ session8.js
@@ -0,0 +1,21 @@
+const items = [  
+    { weight: 2, value: 10 },  
+    { weight: 3, value: 15 },  
+    { weight: 5, value: 40 }  
+  ];  
+  
+const capacity = 7;
+
+
+
+function sumweight(items, capacity) {
+    for(let i = 0; i < items.lenght; i++ ){
+        for(let j = 0; j < items.lenght; j++ ){
+            if(items.weight + items.weight === capacity) {
+            }
+        }
+    }
+}
+
+console.log(sumweight(items, capacity));
+
