d088afc87e50d6a261e47ce5e3566fb0ca26e6a1
DiogoLpes <diogolopes285@gmail.com>
2025-02-19 21:53:41 +0000

    session14

---
+++ session-14/index.js
@@ -0,0 +1,11 @@
+
+import { Sum } from "./sum.js";
+import { Median } from "./sum.js";
+import { Everage } from "./sum.js";
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+console.log(Sum); 
+console.log(Median(arr)); 
+console.log(Everage); 
